d8ee85250baaae5a89aa064acee415caa6e3ccc1
Skip Sauls <skipsauls@gmail.com>
2015-08-20 12:06:39 -0700

    Added support for HTTPS

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var express = require('express');
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
+
 var app = express();
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 5000;
 
 app.set('view engine', 'ejs');
 
@@ -15,5 +19,19 @@ app.get('/oauthcallback', function(req, res) {
 	res.render('oauthcallback', {});
 });
 
-app.listen(port, function() {
-});
+// Create an HTTP service
+http.createServer(app).listen(port);
+console.log("Server listening for HTTP connections on port ", port);
+
+// Create an HTTPS service if the certs are present
+try {
+	var options = {
+	  key: fs.readFileSync('key.pem'),
+	  cert: fs.readFileSync('key-cert.pem')
+	};
+	var https_port = parseInt(port) + 1;
+	https.createServer(options, app).listen(https_port);
+	console.log("Server listening for HTTPS connections on port ", https_port);
+} catch (e) {
+	console.error("Security certs not found, HTTPS not available");
+}
